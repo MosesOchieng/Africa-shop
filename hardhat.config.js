1da20ad928c9ecce1eb29f37ff5ee599799d8650
Steve Kimoi <moenisteve@gmail.com>
2023-04-29 22:52:20 +0300

    structiring config file and deployed contract addresses

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,21 @@
-require("@nomicfoundation/hardhat-toolbox");
+/**
+* @type import('hardhat/config').HardhatUserConfig
+*/
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+
+const { ALCHEMY_API_KEY, PRIVATE_KEY } = process.env;
+
 module.exports = {
-  solidity: "0.8.9",
-};
+  solidity: "0.8.19",
+  defaultNetwork: "polygon_mumbai",
+  networks: {
+    hardhat: {},
+    polygon_mumbai: {
+      url: ALCHEMY_API_KEY,
+      accounts: [PRIVATE_KEY]
+    }
+  },
+}
+
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,32 +1,32 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const { ethers } = require("hardhat"); 
+require("dotenv").config({ path: ".env" }); 
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const daoContractFactory = await ethers.getContractFactory("FarmDAO");
+  const marketplaceContractFactory = await ethers.getContractFactory("Marketplace"); 
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
+  const daoContract = await daoContractFactory.deploy("FarmDAO Contract"); 
+  await daoContract.deployed(); 
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  const marketplaceContract = await marketplaceContractFactory.deploy(daoContract.address);
+  await marketplaceContract.deployed();
+  
+  console.log("FarmDAO contract address: ", daoContract.address);
+  console.log("Marketplace contract address: ", marketplaceContract.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
+// FarmDAO contract address:  0x14D4D952E641b8837408a087b20661885Bc94142
+// Marketplace contract address:  0x3412b425Cd05968DF71119C2B02deDe6e6CEDDA2
+
+// FarmDAO contract address:  0x50d13F167AA9EB92e9e7D70425939bAF0370Baf7
+// Marketplace contract address:  0xa144EB01347ce7E3D7D5adf2B1F0DbEE275Ebe4A
+
+// FarmDAO contract address:  0x1092dF9273f46a10d9f497cec73610D32d8D9BD9
+// Marketplace contract address:  0x0B73D490fF0368B4bDF6137F82afAC21E1FB5625
