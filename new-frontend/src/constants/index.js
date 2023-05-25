ee904fd517ba6cd190b5a09fd1bf4d942fd191ed
Stephen Kimoi <moenisteve@gmail.com>
2023-05-25 10:05:59 +0300

    ETH to BTC contract address

--- new-frontend/src/constants/index.js
+++ new-frontend/src/constants/index.js
@@ -4,7 +4,7 @@ import {abi as aggregatorABI} from '../contractABIs/aggregatorV3InterfaceABI.jso
 
 export const farmdaoContractAddress = "0x0D8c3Cc50DFE68e92b9d18fdf2d1aEeb58000A19"
 export const marketplaceContractAddress = "0x8A5516666FA751600105EA9384d6BC92C461a32D"
-export const aggregatorV3InterfaceAddress = "0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43"
+export const aggregatorV3InterfaceAddress = "0x694AA1769357215DE4FAC081bf1f309aDC325306"
 export const farmDaoAbi = farmDaoABI;  
 export const marketplaceAbi = marketplaceABI; 
 export const aggregatorAbi = aggregatorABI; 
--- new-frontend/src/contractInstance.js
+++ new-frontend/src/contractInstance.js
@@ -1,5 +1,5 @@
 import { Provider } from "@ethersproject/providers";
-import { marketplaceAbi, farmDaoAbi, farmdaoContractAddress, marketplaceContractAddress } from "./constants";
+import { marketplaceAbi, farmDaoAbi, farmdaoContractAddress, marketplaceContractAddress, aggregatorAbi, aggregatorV3InterfaceAddress } from "./constants";
 import { ethers } from "ethers";
 
 const getProviderOrSigner = async (needSigner = false) => {
@@ -7,21 +7,28 @@ const getProviderOrSigner = async (needSigner = false) => {
 
     let farmDaoContract; 
     let marketplaceContract; 
+    let priceFeed; 
     
     const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const aggregatorProvider = new ethers.providers.JsonRpcProvider("https://rpc.ankr.com/eth_sepolia")
 
-    const network = await provider.getNetwork(); 
+    // const network = await provider.getNetwork(); 
     const signer = await provider.getSigner(); 
+
+    // Testing whether the aggregator code matches the latest version
+    // const test = await aggregatorProvider.getCode(aggregatorV3InterfaceAddress); 
 
     if (needSigner) {
       farmDaoContract = new ethers.Contract(farmdaoContractAddress, farmDaoAbi, signer); 
       marketplaceContract = new ethers.Contract(marketplaceContractAddress, marketplaceAbi, signer); 
+      priceFeed = new ethers.Contract(aggregatorV3InterfaceAddress, aggregatorAbi, aggregatorProvider); 
     } else {
-        farmDaoContract = new ethers.Contract(farmdaoContractAddress, farmDaoAbi, provider); 
-        marketplaceContract = new ethers.Contract(marketplaceContractAddress, marketplaceAbi, provider); 
+      farmDaoContract = new ethers.Contract(farmdaoContractAddress, farmDaoAbi, provider); 
+      marketplaceContract = new ethers.Contract(marketplaceContractAddress, marketplaceAbi, provider);
+      priceFeed = new ethers.Contract(aggregatorV3InterfaceAddress, aggregatorAbi, aggregatorProvider); 
     }
 
-    return { farmDaoContract, marketplaceContract }
+    return { farmDaoContract, marketplaceContract, priceFeed }
   } catch (error) {
     console.error(error)
   }
