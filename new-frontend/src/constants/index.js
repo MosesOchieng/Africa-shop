c445f13697c69b15c5c3f532eaf85e18778385f4
Stephen Kimoi <moenisteve@gmail.com>
2023-06-20 17:16:23 +0300

    new contract abi and address

--- new-frontend/src/constants/index.js
+++ new-frontend/src/constants/index.js
@@ -3,7 +3,7 @@ import { abi as marketplaceABI} from '../contractABIs/Marketplace.json'
 import {abi as aggregatorABI} from '../contractABIs/aggregatorV3InterfaceABI.json'
 import { abi as priceConsumerABI} from '../contractABIs/PriceConsumerV3.json'; 
 
-export const farmdaoContractAddress = "0xD1Bec5b7ea429Cb8c73618b19F3052804fA16d86"
+export const farmdaoContractAddress = "0x24899938e935D1E614aC19A5C1484d6041f6F83d"
 export const marketplaceContractAddress = "0xe2dEeAECD5A0D7e6E8f55090F4F8521c5373F17F"
 export const aggregatorV3InterfaceAddress = "0x694AA1769357215DE4FAC081bf1f309aDC325306"
 export const priceConsumerV3Address = "0x1ad9d4269E0Ce5b4c30483244c261F31fbD5b6f5"
